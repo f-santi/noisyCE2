041567343354a8c118516b4e8f0fa83c49e6804e
Flavio Santi <flavio.santi@unitn.it>
2019-08-21 10:29:16 +0200

    Added tests for `ts_change`

--- tests/testthat/test_stoprules.R
+++ tests/testthat/test_stoprules.R
@@ -25,3 +25,13 @@ test_that('ts_change works for reltol argument', {
 })
 
 
+test_that('ts_change works for arguments tol and reltol', {
+  expect_identical(as.numeric(ts_change(c(   0,    2), tol = 3)), 0)
+  expect_identical(as.numeric(ts_change(c(   0,   -3), tol = 3)), 1)
+  expect_identical(as.numeric(ts_change(c( 100,  101), tol = 2, reltol = 0.03)), 0)
+  expect_identical(as.numeric(ts_change(c( 100,  102), tol = 2, reltol = 0.03)), 0)
+  expect_identical(as.numeric(ts_change(c( 100,  102), tol = 4, reltol = 0.01)), 0)
+  expect_identical(as.numeric(ts_change(c( 100,  104), tol = 2, reltol = 0.03)), 1)
+})
+
+
